f47bdd7d2e7d7cb427264bb51a9446bccfa8469b
Karl-Erik Kley <karl-erik.kley@fh-erfurt.de>
2018-11-11 22:07:47 +0100

    Add mysql init

--- mysql/sql_scripts/startup.sql
+++ mysql/sql_scripts/startup.sql
@@ -1,12 +1,8 @@
-CREATE  DATABASE smhoezma;
-
-USE smhoezma;
-
 CREATE TABLE Devices (
 	DeviceID      INT NOT NULL AUTO_INCREMENT,
 	Name          VARCHAR(255) NOT NULL,
 	IP            VARCHAR(255) NOT NULL,
-	GeräteNummer  DECIMAL NOT NULL,
+	GeraeteNummer  DECIMAL NOT NULL,
 	Settings      VARCHAR(255) NOT NULL,
 	PRIMARY KEY (DeviceID)
 );
