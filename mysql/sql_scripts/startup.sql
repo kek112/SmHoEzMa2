933e4126651df99e0f9769adfb29f60fa7784261
Karl-Erik Kley <karl-erik.kley@fh-erfurt.de>
2018-11-12 19:34:08 +0100

    Modify SQL init statement

--- mysql/sql_scripts/startup.sql
+++ mysql/sql_scripts/startup.sql
@@ -2,7 +2,7 @@ CREATE TABLE Devices (
 	DeviceID      INT NOT NULL AUTO_INCREMENT,
 	Name          VARCHAR(255) NOT NULL,
 	IP            VARCHAR(255) NOT NULL,
-	GeraeteNummer  INTEGER NOT NULL,
+	GeraeteNummer  INT NOT NULL,
 	Settings      VARCHAR(255) NOT NULL,
 	PRIMARY KEY (DeviceID)
 );
