3b4ee5872fe778e4c4534a59fcd41d52fc0153dc
Karl-Erik Kley <karl-erik.kley@fh-erfurt.de>
2018-11-12 19:32:35 +0100

    Add SQL query to get all devices through sql connector

--- API_SmHoEzMa/app.py
+++ API_SmHoEzMa/app.py
@@ -6,14 +6,21 @@ import mysql.connector
 app = Flask(__name__)
 
 
-def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
+
+#### QUERY SECTION ####
+
+query_get_all_devices = ("SELECT * FROM Devices")
 
 
-def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
+#### QUERY SECTION ####
 
 
+# important dont ferget to close the connction through obj.close() at the end of your function
 def getMysqlConnection():
-    return mysql.connector.connect(user='testing', host='db', port='3306', password='testing', database='test')
+    #complete
+    #return mysql.connector.connect(user='root', host='db', port='3306', password='test', database='smhoezma')
+    #dev
+    return mysql.connector.connect(user='root', host='192.168.178.30', port='3306', password='test', database='smhoezma')
 
 
 class Payload(object):
@@ -28,38 +35,32 @@ def hello():
 
 @app.route('/test', methods=['GET', 'POST'])
 def get_tasks():
-    received_data = json2obj(request.data)
-
     # load json file which contains node
     with open('nodes.json', 'r') as myfile:
-        data = myfile.read().replace('\n', '')
-
-    loaded_data = data
-    saved_data = json2obj(loaded_data)
-    json_obj = compare_json_data(received_data, saved_data)
-
-    if json_obj:
         return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
-    else:
-        # add json obj to string
-        saved_data.append(received_data)
-        # save json structure back to file
-        return json.dumps({'success': False}), 400, {'ContentType': 'application/json'}
+    return json.dumps({'success': False}), 400, {'ContentType': 'application/json'}
 
     # return saved_data[0].ip
 
 
-# open nodes json and dump everthing to client
+# connect to mysql server inside docker network and get all devices return them as described in readme
 # will return whole list of devices
 @app.route('/api/alldevices', methods=['GET'])
 def return_all_devices():
     try:
-        with open('../nodes.json', 'r') as myfile:
-            data = myfile.read().replace('\n', '')
-        return_json = json2obj(data)
-        return jsonify(return_json)
+        mysql_obj= getMysqlConnection()
+        cur = mysql_obj.cursor()
+        cur.execute(query_get_all_devices)
+
+        row_headers = [x[0] for x in cur.description]  # this will extract row headers
+        rv = cur.fetchall()
+        json_data = []
+        for result in rv:
+            json_data.append(dict(zip(row_headers, result)))
+        return json.dumps(json_data)
+
     except IOError:
-        return json.dumps({'success': False, 'Errorcode': 'File could not be opened'}), 400, {'ContentType': 'application/json'}
+        return json.dumps({'success': False}), 400, {'ContentType': 'application/json'}
 
 
 # return the object of one specific device which is found by the ip
@@ -73,26 +74,14 @@ def return_one_devices():
     try:
         with open('../nodes.json', 'r') as myfile:
             data = myfile.read().replace('\n', '')
-        saved_data = json2obj(data)
-        return_json = find_object_by_ip(device_ip, saved_data)
-        return jsonify(return_json)
     except IOError:
         return json.dumps({'success': False, 'Errorcode': 'Please enter valid ip'}), 400, {'ContentType': 'application/json'}
 
 
 # just return all the sensor values
 @app.route('/api/sensor', methods=['POST'])
-def return_one_devices():
-    data = request.get_json()
-    device_ip = data['Ip']
-    try:
-        with open('../nodes.json', 'r') as myfile:
-            data = myfile.read().replace('\n', '')
-        saved_data = json2obj(data)
-        return_json = find_object_by_ip(device_ip, saved_data)
-        return jsonify(return_json)
-    except IOError:
-        return json.dumps({'success': False, 'Errorcode': 'Please enter valid ip'}), 400, {'ContentType': 'application/json'}
+def return_sensors():
+    return json.dumps({'success': False, 'Errorcode': 'Please enter valid ip'}), 400, {'ContentType': 'application/json'}
 
 
 def find_object_by_ip(ip, saved_data):
--- mysql/sql_scripts/startup.sql
+++ mysql/sql_scripts/startup.sql
@@ -2,7 +2,7 @@ CREATE TABLE Devices (
 	DeviceID      INT NOT NULL AUTO_INCREMENT,
 	Name          VARCHAR(255) NOT NULL,
 	IP            VARCHAR(255) NOT NULL,
-	GeraeteNummer  DECIMAL NOT NULL,
+	GeraeteNummer  INTEGER NOT NULL,
 	Settings      VARCHAR(255) NOT NULL,
 	PRIMARY KEY (DeviceID)
 );
