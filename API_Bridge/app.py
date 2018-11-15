02b914cd64b762b909c974afaff2d7bcd9b046ad
Martin Lesser <mlesser@inoxio.de>
2018-11-15 19:34:46 +0100

    Fix update_lamp_states
    The routine checks if the lamp already exists in the db.
    if true the db entry will be updated else it will be inserted.

--- API_Bridge/app.py
+++ API_Bridge/app.py
@@ -21,6 +21,7 @@ class Payload(object):
 def hello():
     return "Hello World!"
 
+
 @app.route('/print_lamp_states', methods=['GET', 'POST'])
 def print_lamp_states():
     lamp_states = get_lamp_states()
@@ -32,6 +33,7 @@ def print_lamp_states():
     string += '</table>'
     return string
 
+
 @app.route('/update_lamp_states', methods=['GET', 'POST'])
 def update_lamp_states():
     lamp_states = get_lamp_states()
@@ -40,22 +42,38 @@ def update_lamp_states():
     for lamp_number in lamp_states:
         settings = re.sub('u\'', '', str(lamp_states[lamp_number]['state']))
         settings = re.sub('\'', '', settings)
-        values = "'Lampe "+str(lamp_number)+"', '"+str(ip)+"', '"+str(lamp_number)+"', '"+settings+"'"
-        # TODO: UPDATE when device already in db
-        sql = 'INSERT INTO Devices (Name, IP, GeraeteNummer, Settings) VALUES ('+values+')'
+        name = "Lamp "+str(lamp_number)
+        values = "'" + name + "'" + "', '" + str(ip) + "', '" + str(lamp_number) + "', '" + settings + "'"
+        lamp_exists = is_lamp_in_db(cur, lamp_number)
+        if lamp_exists:
+            sql = "UPDATE Devices " \
+                  "SET Name='"+name+"', IP='"+ip+"', Settings='"+settings+"' "\
+                  "WHERE GeraeteNummer="+lamp_number
+        else:
+            sql = 'INSERT INTO Devices (Name, IP, GeraeteNummer, Settings) VALUES ('+values+')'
         print sql
         cur.execute(sql)
         mysql_obj.commit()
     mysql_obj.close()
-    return 'All lamps were inserted!'
+    return 'All lamps were inserted or updated!'
+
+
+def is_lamp_in_db(cursor, lamp_number):
+    assert cursor is not None
+    sql = 'SELECT COUNT(*) FROM Devices WHERE GeraeteNummer='+lamp_number
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    return True if result[0] > 0 else False
+
 
 def get_lamp_states():
     r = requests.get('http://'+ip+':'+port+'/api/newdeveloper/lights/').json()
     return r
 
+
 @app.route('/update_lamp', methods=['GET', 'POST'])
 def update_lamp():
-    settings = {"hue": 34, "on": False, "bri": 56}
+    settings = {"hue": 34, "on": True, "bri": 56}
     payload = json.dumps(settings)
     r = requests.put('http://'+ip+':'+port+'/api/newdeveloper/lights/1/state', data=payload)
     return "Status-Code: "+str(r.status_code)
