194a1742f8dd05700cd6eb3e02ae54a9d0828e1b
Karl-Erik Kley <karl-erik.kley@fh-erfurt.de>
2018-11-28 22:12:52 +0100

    Add API Functions for SmHoEzMa
    New sql statements which are now clearer through splitting data and the query
    new needed no response execute function
    added more functionality for lamp
     * responds to post and get

--- API_SmHoEzMa/app.py
+++ API_SmHoEzMa/app.py
@@ -8,7 +8,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+
+    query = """UPDATE Devices SET Name = %s WHERE IP = %s"""
+    data = ("bobby", "1.2.3.4")
+    send_query_to_db_no_response(query, data)
+    return "Hello World"
 
 
 # connect to mysql server inside docker network and get all devices return them as described in readme
@@ -16,8 +20,9 @@ def hello():
 @app.route('/api/alldevices', methods=['GET'])
 def return_all_devices():
     query_get_all_devices = "SELECT * FROM Devices"
+    sql_data = ""
     try:
-        return send_query_to_db(query_get_all_devices)
+        return send_query_to_db(query_get_all_devices, sql_data)
     except IOError:
         return json.dumps({'success': False}), 400, {'ContentType': 'application/json'}
 
@@ -25,23 +30,57 @@ def return_all_devices():
 # return the object of one specific device which is found by the ip
 #{
 #  "IP": "10.10.10.10"
+#  "GeraeteNummer": "1"
 #}
 @app.route('/api/device', methods=['Get','POST'])
 def return_one_devices():
     data = request.get_json()
-    query_specific_device = "SELECT * FROM `Devices` WHERE IP = \'" + data['IP'] + "\'"
+    query_specific_device = """SELECT * FROM Devices WHERE IP =%s AND GeraeteNummer = %s """
+    sql_data = ( data['IP'], data['GeraeteNummer'])
+
     try:
-        return send_query_to_db(query_specific_device)
+        return send_query_to_db(query_specific_device,sql_data)
     except IOError:
         return json.dumps({'success': False, 'Errorcode': 'Please enter valid ip'}), 400, {'ContentType': 'application/json'}
 
 
 # just return all the lamps
-@app.route('/api/lamp', methods=['GET'])
+@app.route('/api/lamp', methods=['GET','POST'])
 def return_lamps():
-    query_get_lamp = "SELECT * FROM `Devices` WHERE `GeraeteNummer` != 0"
+    data = request.get_json()
+    if request.method == 'GET':
+
+        query_get_lamp = """SELECT * FROM Devices WHERE GeraeteNummer != 0"""
+        sql_data = ""
+        try:
+            return send_query_to_db(query_get_lamp, sql_data)
+        except IOError:
+            return json.dumps({'success': False}), 400, {'ContentType': 'application/json'}
+
+    if request.method == 'POST':
+        query_lamp = """UPDATE Devices SET  Colour = %s, Saturation = %s, Brightness = %s, Switch = %s WHERE IP = %s AND GeraeteNummer = %s """
+        sql_data = (data['Hue'], data['Saturation'], data['Brightness'], data['Switch'], data['IP'], data['GeraeteNummer'])
+
+        try:
+            send_query_to_db_no_response(query_lamp, sql_data)
+            #
+            # TODO: API CALL FOR BRIDGE API
+            #
+            return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+        except IOError:
+            return json.dumps({'success': False}), 400, {'ContentType': 'application/json'}
+
+
+@app.route('/api/name', methods=['POST'])
+def set_name():
+    data = request.get_json()
+
+    query_update_name = """UPDATE Devices SET Name = %s  WHERE IP = %s  AND GeraeteNummer = %s"""
+    sql_data = (data['Name'], data['IP'], data['GeraeteNummer'])
+
     try:
-        return send_query_to_db(query_get_lamp)
+        send_query_to_db_no_response(query_update_name, sql_data)
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
     except IOError:
         return json.dumps({'success': False}), 400, {'ContentType': 'application/json'}
 
@@ -49,20 +88,29 @@ def return_lamps():
 # just return all the sensor values
 @app.route('/api/sensor', methods=['GET'])
 def return_sensors():
-    query_get_sensor = "SELECT * FROM `Devices` WHERE `GeraeteNummer` = 0"
+
+    query_get_sensor = """SELECT * FROM Devices WHERE GeraeteNummer = 0 """
+    sql_data = ""
     try:
-        return send_query_to_db(query_get_sensor)
+        return send_query_to_db(query_get_sensor,sql_data)
     except IOError:
         return json.dumps({'success': False}), 400, {'ContentType': 'application/json'}
 
 
-def send_query_to_db(query):
+def send_query_to_db_no_response(query, data):
     mysql_obj = get_mysql_connection()
     cur = mysql_obj.cursor()
-    cur.execute(query)
+    cur.execute(query, data)
+    mysql_obj.close()
+
+
+def send_query_to_db(query, data):
+    mysql_obj = get_mysql_connection()
+    cur = mysql_obj.cursor()
+    cur.execute(query, data)
 
     row_headers = [x[0] for x in cur.description]  # this will extract row headers
-    rv = cur.fetchall()  # ectract data
+    rv = cur.fetchall()  # extract data
     mysql_obj.close()  # close connection
 
     json_data = []
@@ -71,12 +119,12 @@ def send_query_to_db(query):
     return json.dumps(json_data)
 
 
-# important dont ferget to close the connction through obj.close() at the end of your function
+# important dont forget to close the connction through obj.close() at the end of your function
 def get_mysql_connection():
     #live
     #return mysql.connector.connect(user='root', host='db', port='3306', password='test', database='smhoezma')
     #dev
-    return mysql.connector.connect(user='root', host='192.168.178.30', port='3306', password='test', database='smhoezma')
+    return mysql.connector.connect(user='root', host='192.168.178.30', port='3306', password='test', database='smhoezma', autocommit=True)
 
 
 if __name__ == '__main__':
