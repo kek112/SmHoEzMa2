19ab75736a454303bb9ab90cd8af86ed519a396d
Karl-Erik Kley <karl-erik.kley@fh-erfurt.de>
2019-01-16 17:55:44 +0100

    Fix

--- API_SmHoEzMa/app.py
+++ API_SmHoEzMa/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def hello():
 
     query = """UPDATE Devices SET Name = %s WHERE IP = %s"""
-    data = ("testinsert", "1.2.3.4")
+    data = ("testinsert", "test")
     mysql_helper.send_query_to_db_no_response(query, data)
     return "Hello World"
 
@@ -19,18 +19,19 @@ def hello():
 # will return whole list of devices
 @app.route('/api/alldevices', methods=['GET'])
 def return_all_devices():
-    query_get_all_devices = "SELECT * FROM Devices"
+    query_get_all_devices = """SELECT * FROM Devices"""
     sql_data = ""
     try:
-        return send_query_to_db(query_get_all_devices, sql_data)
+        return json.dumps(mysql_helper.send_query_to_db(query_get_all_devices, sql_data))
     except IOError:
         return json.dumps({'success': False}), 400, {'ContentType': 'application/json'}
 
-@app.route('/api/device', methods=['GET','POST'])
+
+@app.route('/api/device', methods=['GET', 'POST'])
 def return_one_devices():
     data = request.get_json()
-    query_specific_device = """SELECT * FROM Devices WHERE IP =%s AND GeraeteNummer = %s """
-    sql_data = ( data['IP'], data['GeraeteNummer'])
+    query_specific_device = """SELECT * FROM Devices WHERE IP = '%s' AND GeraeteNummer = %s """
+    sql_data = (data['IP'], data['GeraeteNummer'])
 
     try:
         return json.dumps(mysql_helper.send_query_to_db_no_response(query_specific_device, sql_data))
@@ -39,7 +40,7 @@ def return_one_devices():
 
 
 # just return all the lamps
-@app.route('/api/lamp', methods=['GET','POST'])
+@app.route('/api/lamp', methods=['GET', 'POST'])
 def return_lamps():
     data = request.get_json()
     if request.method == 'GET':
@@ -89,37 +90,6 @@ def return_sensors():
         return json.dumps(mysql_helper.send_query_to_db_no_response(query_get_sensor, sql_data))
     except IOError:
         return json.dumps({'success': False}), 400, {'ContentType': 'application/json'}
-
-
-def send_query_to_db_no_response(query, data):
-    mysql_obj = get_mysql_connection()
-    cur = mysql_obj.cursor()
-    cur.execute(query, data)
-    mysql_obj.close()
-
-
-def send_query_to_db(query, data):
-    mysql_obj = get_mysql_connection()
-    cur = mysql_obj.cursor()
-    cur.execute(query, data)
-
-    row_headers = [x[0] for x in cur.description]  # this will extract row headers
-    rv = cur.fetchall()  # extract data
-    mysql_obj.close()  # close connection
-
-    json_data = []
-    for result in rv:
-        json_data.append(dict(zip(row_headers, result)))
-    return json_data
-
-
-def get_mysql_connection():
-    # live
-    # return mysql.connector.connect(user='root', host='db', port='3306', password='test', database='smhoezma')
-    # martin
-    #return mysql.connector.connect(user='root', host='192.168.0.2', port='3306', password='test', database='smhoezma')
-    # karl
-    return mysql.connector.connect(user='root', host='192.168.178.30', port='3306', password='test', database='smhoezma', autocommit=True)
 
 
 if __name__ == '__main__':
